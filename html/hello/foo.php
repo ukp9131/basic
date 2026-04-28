918ba80e53d231d0e1fbc1b08ea6fba97efd4254
ukp9131 <azb4472@naver.com>
2026-04-28 13:47:55 +0900

    2-2

---
+++ html/hello/foo.php
@@ -0,0 +1,6 @@
+<a href="https://ukptest2.dothome.co.kr/world/bar.php">외부경로</a>
+<a href="/world/bar.php">절대경로</a>
+<a href="../world/bar.php">상대경로</a>
+<img src="https://ukptest2.dothome.co.kr/world/dog.jpeg">
+<img src="/world/dog.jpeg">
+<img src="../world/dog.jpeg">
