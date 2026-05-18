e09933bf115f380dbab37155fdbc3af8d735b1f4
ukp9131 <azb4472@naver.com>
2026-05-18 17:02:21 +0900

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,4 +1,11 @@
-<a href="https://naver.com" target="_blank">
-    네이버
-</a>
-<img src="cat.jpeg">
+<!DOCTYPE html><!-- html5를 사용하겠다는 의미 -->
+<html lang="ko"><!-- 가장 상위 태그, lang 속성값은 페이지의 언어를 의미 -->
+<head><!-- html 자식태그 -->
+    <meta charset="UTF-8"><!-- 문자셋 설정, 한글깨짐 방지 -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- 모바일에서 잘보이도록 설정 -->
+    <title>언노운팝스</title><!-- 웹페이지 제목 -->
+</head>
+<body><!-- html 자식태그, 실제로 보여지는 태그 작성 -->
+    <img src="cat.jpeg">
+</body>
+</html>
