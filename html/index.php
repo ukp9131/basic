51686bb75359263740287222f4bf20dc47985698
ukp9131 <azb4472@naver.com>
2026-05-26 11:16:03 +0900

    3-1

--- html/index.php
+++ html/index.php
@@ -4,8 +4,17 @@
     <meta charset="UTF-8"><!-- 문자셋 설정, 한글깨짐 방지 -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- 모바일에서 잘보이도록 설정 -->
     <title>언노운팝스</title><!-- 웹페이지 제목 -->
+    <style>
+        h2 {
+            font-size: 16px;
+            color: green;
+        }
+    </style>
+    <link rel="stylesheet" href="a.css">
 </head>
 <body><!-- html 자식태그, 실제로 보여지는 태그 작성 -->
-    <img src="cat.jpeg">
+    <h1 style="font-size: 12px; color: red;">H1 태그입니다.</h1>
+    <h2>H2 태그입니다.</h2>
+    <h3>H3 태그입니다.</h3>
 </body>
 </html>
